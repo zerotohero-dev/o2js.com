98089cf2f59e3641024eb0ff908020cba5d9edd3
Volkan Özçelik <volkan@o2js.com>
2013-09-20 21:14:55 +0000

    added analytics.

--- server/blog/bin/publish.sh
+++ server/blog/bin/publish.sh
@@ -1,15 +1,3 @@
-echo "Starting publish proces..."
-cd /root/PROJECTS/o2js.com/server/blog
-echo "Restoring configuration files..."
+cd ..
 cp app/views/layouts/application.html.ejs.local app/views/layouts/application.html.ejs
-echo "Temporarily stopping servers..."
-nginx -s stop;apachectl -k stop
-echo "Starting generation process..."
 scotch generate
-echo "Generation done! Starting servers..."
-nginx;apachectl -k start
-echo "Servers started. Creating markdowns..."
-cd bin
-node ./update-markdowns.js
-cd /root/
-echo "All done. Do not forget to publish to github."
